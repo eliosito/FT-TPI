eb71a835f221f3d55249ad141512cd2686cf5b54
eliosito <eliovillarroelsandoval@gmail.com>
2026-09-22 23:55:20 -0300

    Nuevos testeos a las funciones saludar y esmayor de edad

--- src/funciones/funciones.test.js
+++ src/funciones/funciones.test.js
@@ -1,26 +1,66 @@
-import {sumar} from "./funciones.js"
-import {describe, it , test, expect} from 'vitest'
+import { sumar, esMayorDeEdad, saludar } from "./funciones.js";
+import { describe, test, expect } from "vitest";
 
 
-describe("sumar", ()=> {
-test("devuelve 8 cuando suma 5 y 3", () => {
-    const a = 5;
-    const b = 3;
+describe("sumar", () => {
 
+    test("devuelve 8 cuando suma 5 y 3", () => {
+        const a = 5;
+        const b = 3;
 
-    const resultado = sumar(a,b);
+        const resultado = sumar(a, b);
 
-    expect(resultado).toBe(8)
+        expect(resultado).toBe(8);
+    });
+
+    test("devuelve 0 cuando suma dos 0", () => {
+        const a = 0;
+        const b = 0;
+
+        const resultado = sumar(a, b);
+
+        expect(resultado).toBe(0);
+    });
+
 });
 
-test("devuelve 0 cuando suma dos 0", () => {
-    const a = 0;
-    const b = 0;
 
+describe("esMayorDeEdad", () => {
 
-    const resultado = sumar(a,b);
+    test("devuelve true cuando la persona tiene 18 años", () => {
+        const edad = 18;
 
-    expect(resultado).toBe(0)
+        const resultado = esMayorDeEdad(edad);
+
+        expect(resultado).toBe(true);
+    });
+
+    test("devuelve false cuando la persona tiene 17 años", () => {
+        const edad = 17;
+
+        const resultado = esMayorDeEdad(edad);
+
+        expect(resultado).toBe(false);
+    });
+
 });
 
+
+describe("saludar", () => {
+
+    test("devuelve Hola, Elio cuando recibe Elio", () => {
+        const nombre = "Elio";
+
+        const resultado = saludar(nombre);
+
+        expect(resultado).toBe("Hola, Elio");
+    });
+
+    test("devuelve Hola, Juan cuando recibe Juan", () => {
+        const nombre = "Juan";
+
+        const resultado = saludar(nombre);
+
+        expect(resultado).toBe("Hola, Juan");
+    });
 });
